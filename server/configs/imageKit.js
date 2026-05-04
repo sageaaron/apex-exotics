fab3830cf34a7078be3a0d0a7a66b1382a194987
sageaaron <sageaaronpadayachee@gmail.com>
2026-05-04 13:36:52 +0200

    fix: add public key to ImageKit config

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,6 +1,7 @@
 import ImageKit from "@imagekit/nodejs";
 
 const imageKit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
